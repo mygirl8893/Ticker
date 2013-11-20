35e70adc91ca6ac3706720c218b8081ed7067a67
Mert Dümenci <mert@dumenci.me>
2013-11-20 20:31:10 +0200

    Add descriptive labels to the graph

--- Ticker/BTCGraphView.m
+++ Ticker/BTCGraphView.m
@@ -10,11 +10,15 @@
 
 @interface BTCGraphView (private)
 
+- (void)sizeToFitDescriptiveLabels;
 - (UIBezierPath *)pathWithValues:(NSArray *)values;
 
 @end
 
-@implementation BTCGraphView
+@implementation BTCGraphView {
+    UILabel *_maxLabel;
+    UILabel *_minLabel;
+}
 
 - (id)initWithFrame:(CGRect)frame
 {
@@ -22,6 +26,18 @@
     if (self) {
         self.graphLayer.frame = self.bounds;
         [self.layer addSublayer:self.graphLayer];
+        
+        _maxLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 0, 50, 20)];
+        _minLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, frame.size.height - 20, 50, 20)];
+        
+        _maxLabel.backgroundColor = _minLabel.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:0.5f];
+        _maxLabel.textColor = _minLabel.textColor = [UIColor blackColor];
+        _maxLabel.font = _minLabel.font = [UIFont fontWithName:@"AvenirNext-DemiBold" size:12.f];
+        _maxLabel.layer.cornerRadius = _minLabel.layer.cornerRadius = 3.f;
+        _maxLabel.textAlignment = _minLabel.textAlignment = NSTextAlignmentCenter;
+        
+        [self addSubview:_minLabel];
+        [self addSubview:_maxLabel];
     }
     return self;
 }
@@ -43,44 +59,67 @@
     if (_values != values) {
         _values = values;
         
+        _minLabel.text = [[values valueForKeyPath:@"@min.integerValue"] stringValue];
+        _maxLabel.text = [[values valueForKeyPath:@"@max.integerValue"] stringValue];
+        
+        [self sizeToFitDescriptiveLabels];
+    
         UIBezierPath *graphPath = [self pathWithValues:values];
         
-        CABasicAnimation *graphAnimation = [CABasicAnimation animationWithKeyPath:@"path"];
-        graphAnimation.fromValue = (id)self.graphLayer.path;
-        graphAnimation.toValue = (id)graphPath.CGPath;
-        graphAnimation.duration = 1.f;
+        CATransition *graphFade = [CATransition animation];
+        [graphFade setDuration:0.3];
+        [graphFade setType:kCATransitionFade];
+        [graphFade setTimingFunction:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut]];
         
-        [self.graphLayer addAnimation:graphAnimation forKey:@"graphAnimation"];
+        [self.graphLayer addAnimation:graphFade forKey:kCATransitionFade];
         self.graphLayer.path = graphPath.CGPath;
     }
 }
 
 #pragma mark - Private
 
-- (UIBezierPath *)pathWithValues:(NSArray *)values {
-    NSNumber *maxValue = [values valueForKeyPath:@"@max.floatValue"];
+- (void)sizeToFitDescriptiveLabels {
+    [_minLabel sizeToFit];
+    [_maxLabel sizeToFit];
     
-    CGFloat scale = self.bounds.size.height;
+    _minLabel.frame = CGRectMake(_minLabel.frame.origin.x, _minLabel.frame.origin.y, _minLabel.frame.size.width + 5, _minLabel.frame.size.height);
+    _maxLabel.frame = CGRectMake(_maxLabel.frame.origin.x, _maxLabel.frame.origin.y, _maxLabel.frame.size.width + 5, _maxLabel.frame.size.height);
+}
+
+- (UIBezierPath *)pathWithValues:(NSArray *)values {
+    CGFloat maxValue = [[values valueForKeyPath:@"@max.floatValue"] floatValue];
+    CGFloat minValue = [[values valueForKeyPath:@"@min.floatValue"] floatValue];
+    
+    CGFloat scale = 1.f;
     CGFloat xThreshold = self.bounds.size.width / (values.count - 1);
+    
+    CGPoint graphPoint = CGPointZero;
     
     UIBezierPath *path = [UIBezierPath bezierPath];
     path.lineWidth = 1.f;
     
-    for (NSInteger i = 0; i < values.count; i++) {
-        NSNumber *value = values[i];
-        CGFloat normalizedValue = value.floatValue * scale / maxValue.floatValue;
+    if (maxValue == minValue) {
+        graphPoint = CGPointMake(self.frame.size.width, self.frame.size.height / 2);
         
-        CGFloat x = values.count == 1 ? self.bounds.size.width : xThreshold * i;
-        CGPoint graphPoint = CGPointMake(x, self.bounds.size.height - normalizedValue);
-
-        if (values.count == 1) {
-            [path moveToPoint:CGPointMake(0, graphPoint.y)];
-            [path addLineToPoint:graphPoint];
-        } else {
-            if (i == 0) {
-                [path moveToPoint:graphPoint];
-            } else {
+        [path moveToPoint:CGPointMake(0, graphPoint.y)];
+        [path addLineToPoint:graphPoint];
+    } else {
+        for (NSInteger i = 0; i < values.count; i++) {
+            CGFloat value = [values[i] floatValue];
+            CGFloat normalizedValue = ((value - minValue) * (self.bounds.size.height * scale)) / (MAX(maxValue - minValue, 1));
+            
+            CGFloat x = values.count == 1 ? self.bounds.size.width : xThreshold * i;
+            graphPoint = CGPointMake(x, self.bounds.size.height - normalizedValue);
+            
+            if (values.count == 1) {
+                [path moveToPoint:CGPointMake(0, graphPoint.y)];
                 [path addLineToPoint:graphPoint];
+            } else {
+                if (i == 0) {
+                    [path moveToPoint:graphPoint];
+                } else {
+                    [path addLineToPoint:graphPoint];
+                }
             }
         }
     }
